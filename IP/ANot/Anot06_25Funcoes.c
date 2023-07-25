5fd7be9c7e28202c4af3a8067e5e61fcaf3816ff
Arturvpf <arturvpf3@gmail.com>
2023-07-25 14:54:57 -0300

    anot

--- IP/ANot/Anot06_25Funcoes.c
+++ IP/ANot/Anot06_25Funcoes.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-void preenchev(int *v,int tam){
-	int i;
-	for(i=0;i<tam;i++){
-		printf("Digite o elemento %d",i+1);
-		scanf("%d",&v[i]);
+int *preenche(int tam){
+	int *vet;
+	if((vet=(int *) malloc(sizeof(int)*tam))==NULL){
+		exit(1);
 	}
+	for(int i=0;i<tam;i++){
+		scanf("%d",&vet[i]);
+	}
+	
+	return vet;
 }
-
 int main(){
-	int v[5],w[15],i;
-	float media;
-	preenchev(v,5);
-	preenchev(w,15);
+	int *vet;
+	vet=preenche(5);
+	for(int i=0;i<5;i++)
+	printf("%d",vet[i]);
+	free(vet);
 }
