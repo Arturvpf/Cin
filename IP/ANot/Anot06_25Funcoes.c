4e3b69c5cd4c6d6fe64406d509c7873272b632aa
Arturvpf <arturvpf3@gmail.com>
2023-07-25 13:50:02 -0300

    anot

--- IP/ANot/Anot06_25Funcoes.c
+++ IP/ANot/Anot06_25Funcoes.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-float mediaV(int *vet,int tam){
-	float soma=0;
-	for(int i=0;i<tam;i++) soma+=vet[i];
-	return soma/tam;
-}
-void melhores(int vet[5], float media){
-	for(int i=0;i<5;i++) if(vet[i]>=media) printf("%d",vet[i]);
+void preenchev(int *v,int tam){
+	int i;
+	for(i=0;i<tam;i++){
+		printf("Digite o elemento %d",i+1);
+		scanf("%d",&v[i]);
+	}
 }
 
 int main(){
-	int i,v[5];
-	for(i=0;i<5;i++){
-		scanf("%d",&v[i]);
-	}
-	float media=mediaV(v,5);
-	melhores(v,media);
+	int v[5],w[15],i;
+	float media;
+	preenchev(v,5);
+	preenchev(w,15);
 }
