d5b037e512d73dc79a6bca5ca3b9ee7b826aa334
Arturvpf <arturvpf3@gmail.com>
2023-08-15 08:37:04 -0300

    bibliotecas

---
+++ IP/ANot/Anot08_15Arquivo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include"Bib.h"
+
+int main(){
+    char frase[50];
+    scanf(" %s",frase);
+    mostra(frase);
+
+}
---
+++ IP/ANot/Bib.c
@@ -0,0 +1,4 @@
+//parte de Anot08_15
+void mostra(char* frase){
+    printf("%s\n",frase);
+}
