7f51f35dfc8701e85d76a7511065518eb6f91776
Arturvpf <arturvpf3@gmail.com>
2023-08-16 00:58:54 -0300

    test

--- IP/ANot/Anot08_15Arquivo.c
+++ IP/ANot/Anot08_15Arquivo.c
@@ -1,55 +1,24 @@
 #include<stdio.h>
-#include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 #include"Bib.h"
 
-void salvaVetor(float *dVet,int tam){
+int main() {
+    float vet[]={13,25,33,44,56}, *dVet;
+    int tamV,i;
     FILE *arq;
-    if((arq=fopen("test.bin","w"))!=NULL){
-        fwrite(&tam,sizeof(int),1,arq);
-        fwrite(dVet,sizeof(float),tam,arq);
+    if((arq=fopen("test.bin","r+"))!=NULL) {
+        dVet = carregaVetor(arq, &tamV);
+        printf("Apos a carga inicial\n");
+        printVetor(dVet, tamV);
+        if(dVet!=NULL) {
+            dVet = inserirVetor(dVet, &tamV, vet, 5);
+            printf("Apos a insercao\n");
+            printVetor(dVet, tamV);
+            salvaVetor(arq, dVet, tamV);
+        }
+        free(dVet);
         fclose(arq);
     }
-}
-float* carregaVetor(int* tam){
-    FILE *arq;
-    float* uai=NULL;
-    if((arq=fopen("test.bin","r"))!=NULL){
-        fread(tam,sizeof(int),1,arq);
-        uai=(float *)malloc(sizeof(float)*(*tam));
-        if(uai==NULL){
-            printf("erro de aloc");
-            exit(1);
-            fread(uai,sizeof(float),*tam,arq);
-            fclose(arq);
-        }
-        
-    }
-    return uai;
-}
-float* inserirVetor(float *dVet,int *tamDV,float *vet, int tamV){
-    dVet=(float *) realloc(dVet,sizeof(float)*((*tamDV)+tamV));
-    if(dVet==NULL){exit(1);}
-        for(int i=0;i<tamV;i++){
-            dVet[(*tamDV)+i]=vet[i];
-        }
-        tamDV+=tamV;
-}
-void printVetor(float *dVet,int tam){
-    for(int i=0;i<tam;i++){
-        printf("vetor:%f",dVet[i]);
-    }
-}
-int main(){
-    float vet[]={13,25,33,44,56};
-    float *dVet;
-    int tamV;
-
-    dVet=carregaVetor(&tamV);
-    if(dVet!=NULL){
-        dVet=inserirVetor(dVet,&tamV,vet,5);
-        salvaVetor(dVet,tamV);
-    }
-    printVetor(dVet,tamV);
+    return 0;
 }
--- IP/ANot/Bib.h
+++ IP/ANot/Bib.h
@@ -1,4 +1,7 @@
 //parte de Anot08_15
-void mostra(int vet[]);
+void salvaVetor(FILE *arq, float *vet, int tam);
+float *carregaVetor(FILE *arq, int *tam);
+float *inserirVetor(float *dVet, int *tamDV, float *vet, int tamV);
+void printVetor(float *dVet, int tam);
 
 #include"Bib.c"
