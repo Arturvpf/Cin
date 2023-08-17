a41d360f8957555af34fd350d5009bc1fe6325a8
Arturvpf <arturvpf3@gmail.com>
2023-08-17 08:41:18 -0300

    mp

---
+++ IP/ANotProva/Mp2022.2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct{
+    char Nome_do_aluno[101];
+    char cpf_do_aluno[12];
+    int qtdNotas;
+    int notas;
+}Aluno;
+
+Aluno* carregaAlunos(int *qtdAlunos);
+float* leNotas(int *qtdNotas);
+float* media(float *notas,int qtdNotas);
+void ordena(Aluno *alunos,int qtdAlunos);
+void salvaAlunos(Aluno *alunos,int qtdAlunos);
+
+int main(){
+    
+
+    return 0;
+}
+Aluno* carregaAlunos(int *qtdAlunos){
+
+}
+float* leNotas(int *qtdNotas){
+
+}
+float* media(float *notas,int qtdNotas){
+
+}
+void ordena(Aluno *alunos,int qtdAlunos){
+
+}
+void salvaAlunos(Aluno *alunos,int qtdAlunos){
+
+}
