0bb4cdc9b3e5cc385f139184ea214eff5bcd16d0
Arturvpf <arturvpf3@gmail.com>
2023-12-20 00:08:33 -0300

    merge

--- I-S/threads2.c
+++ I-S/threads2.c
@@ -1,126 +1,103 @@
-#include <stdio.h>
-#include<pthread.h>
 #include<stdlib.h>
+#include<stdio.h>
+#include<pthread.h>
 
+#define max 100
 
-int num_threads;
-int n;
-pthread_t *threads;
-int *ids;
-int *vet;
-int ig;
+typedef struct Data{
+    int low;
+    int high;
+}Data;
 
+int *arr;
 
-typedef struct parameter{
-  int l,r;
-}parameter;
-struct parameter *parameter;
-
-
-void *thread_sort(void* threadid){
-  int arg=*((int *)threadid);
-  mergesort(parameter[*((int *)threadid)].l,parameter[*((int *)threadid)].r);
-  pthread_exit(NULL);
-}
-
-void merge(int p, int q, int r) {
-
-  // Create L ← A[p..q] and M ← A[q+1..r]
-  int n1 = q - p + 1;
-  int n2 = r - q;
-
-  int L[n1], M[n2];
-
-  for (int i = 0; i < n1; i++)
-    L[i] = vet[p + i];
-  for (int j = 0; j < n2; j++)
-    M[j] = vet[q + 1 + j];
-
-  int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
-
-  // Until we reach either end of either L or M, pick larger among
-  // elements L and M and place them in the correct position at A[p..r]
-  while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
-      vet[k] = L[i];
-      i++;
-    } else {
-      vet[k] = M[j];
-      j++;
-    }
-    k++;
-  }
-
-  // When we run out of elements in either L or M,
-  // pick up the remaining elements and put in A[p..r]
-  while (i < n1) {
-    vet[k] = L[i];
-    i++;
-    k++;
-  }
-
-  while (j < n2) {
-    vet[k] = M[j];
-    j++;
-    k++;
-  }
-}
-
-// Divide the array into two subarrays, sort them and merge them
-void mergeSort(int l, int r) {
-    if (l < r) {
-        num_threads += 2;
-        threads = (pthread_t *)realloc(threads, sizeof(pthread_t) * num_threads);
-        parameter = (struct parameter *)realloc(parameter, sizeof(struct parameter) * num_threads);
-        ids=(int *)realloc(ids,sizeof(int)*num_threads);
-        int m = l + (r - l) / 2;
-
-        parameter[num_threads - 2].l = l;
-        parameter[num_threads - 2].r = m;
-        pthread_create(&threads[num_threads - 2], NULL, thread_sort, (void *)(num_threads - 2));
-
-        parameter[num_threads - 1].l = m + 1;
-        parameter[num_threads - 1].r = r;
-        pthread_create(&threads[num_threads - 1], NULL, thread_sort, (void *)(num_threads - 1));
-
-        //mergeSort(l, m);
-        //mergeSort(m + 1, r);
-
-        // Wait for all threads to finish (join threads)
-        for (int i = num_threads - 2; i < num_threads; i++) {
-            pthread_join(threads[i], NULL);
+void merge(int low, int mid, int high)
+{
+    int i, j, k;
+    int n1 = mid - low + 1;
+    int n2 =  high - mid;
+    
+    int left[n1], right[n2];
+    for (i = 0; i < n1; i++)
+        left[i] = arr[low + i];
+    for (j = 0; j < n2; j++)
+        right[j] = arr[mid+ 1+ j];
+    
+    i = 0;
+    j = 0;
+    k = low;
+    while (i < n1 && j < n2)
+    {
+        if (left[i] <= right[j])
+        {
+            arr[k] = left[i];
+            i++;
         }
-
-        merge(l, m, r);
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2)
+    {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+void* mergeSort(void* arg)
+{
+    Data data = *((Data*)arg);
+    int low = data.low;
+    int high = data.high;
+    
+    if (low < high)
+    {
+        int mid = low+(high-low)/2;
+        
+        pthread_t thread1;
+        Data d1 = {low, mid};
+        
+        pthread_t thread2;
+        Data d2 = {mid+1, high};
+        
+        pthread_create(&thread1, NULL, mergeSort, &d1);
+        pthread_create(&thread2, NULL, mergeSort, &d2);
+        
+        pthread_join(thread1, NULL);
+        pthread_join(thread2, NULL);
+        
+        merge(low, mid, high);
     }
 }
 
+int main()
+{  int i, r1,n;
+    printf("Quantidade de numeros a serem ordenados\n");
+    scanf("%d", &n);
+    arr=(int *) malloc(sizeof(int)*n);
+    for(i=0; i<n; i++)
+        scanf("%d", arr[i]);
 
-// Driver program
-int main() {
-  parameter=NULL;
-  printf("Insira a quantidade de números");
-  scanf("%d",&n);
-
-  vet=(int *)malloc(sizeof(int)*n);
-
-  for(int i=0;i<n;i++){
-    scanf("%d",vet[i]);
-  }
-
-  ig=1;
-  num_threads=1;
-  threads=(pthread_t *) malloc(sizeof(pthread_t)*num_threads);
-  ids=(int *)malloc(sizeof(int)*num_threads);
-  parameter=(struct parameter*) malloc(sizeof(struct parameter)*num_threads);
-  
-  parameter[0].l=0;
-  parameter[0].r=n-1;
-  pthread_create(threads[0],NULL,thread_sort,(void *) ids[0]);
-
-  printf("Sorted array: \n");
-  pthread_exit(NULL);
+    Data data = {0, n-1};
+    
+    pthread_t t;
+    
+    pthread_create(&t, NULL, mergeSort, &data);
+    pthread_join(t, NULL);
+    
+    printf("Sorted array is: \n");
+    for (i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
 }
