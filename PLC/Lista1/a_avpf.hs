ecf62154ffde1e0080941bffc28dd1b31faa2512
Artur Vinicius Pereira Fernandes <127784923+Arturvpf@users.noreply.github.com>
2024-12-13 16:44:03 -0300

    Update a_avpf.hs

--- PLC/Lista1/a_avpf.hs
+++ PLC/Lista1/a_avpf.hs
@@ -1,22 +1,35 @@
-import Data.List.Split (splitOn)
-import Text.Printf (printf)
+-- transformar tudo que é ponto e virgula em espaco ok
+-- separar string por espaco ok
+-- fazer uma tupla com a string e o double 
+-- comparar a  sigla com a string ok
+-- somar os doubles que forem iguais ok 
 
--- Função para calcular o total gasto em um mês específico
+trocarCaractere :: String -> String --ok
+trocarCaractere [] = []
+trocarCaractere (a:as) | a == ';' = ' ' : trocarCaractere as
+                       | otherwise = a : trocarCaractere as
+
+dividirString2 :: String -> [String] --funcionando certo
+dividirString2 []  = []
+dividirString2 str = words str
+
+criarTupla :: [String] -> [(String, Double)] -- cria uma lista de tuplas
+criarTupla [] = []
+criarTupla (a:b:c:d:xs) = (b, read d) : criarTupla xs
+
+verificacao :: String -> [(String, Double)] -> [Double]
+verificacao _ [] = []
+verificacao sigla ((a,b):as) | a == sigla = b : verificacao sigla as
+                             | otherwise  = verificacao sigla as
+
 logMes :: String -> String -> Double
-logMes mes fatura = 
-  let
-    -- Divide a fatura em linhas
-    transacoes = splitOn ";" fatura
-    -- Filtra as transações do mês especificado
-    valoresMes = [read (last (splitOn "," t)) :: Double | t <- transacoes, mes `elem` words t]
-  in
-    sum valoresMes
+logMes sigla frase | null sigla || null frase = 0.0
+                   | otherwise = foldl (+) 0 (verificacao sigla (criarTupla (dividirString2 (trocarCaractere frase))))
 
--- Função principal para interagir com a entrada e saída
-main :: IO ()
 main = do
-  mes <- getLine
-  fatura <- getLine
-  let resultado = logMes mes fatura
-  printf "%.1f\n" resultado
--------WRONG----------
+    a <- getLine
+    b <- getLine
+    let result = logMes a b
+    print result
+--JAN
+--14 JAN;Amazon;40.32;15 JAN;Uber;14.84;25 JAN;Uber;34.24;02 FEV;Spotify;8.50;06 FEV;Uber;6.94;05 MAR;Burger;29.90;10 MAR;Burger;24.99;15 MAR;Uci;19.00;08 ABR;Itunes;3.50;13 ABR;Picpay;20.00;
