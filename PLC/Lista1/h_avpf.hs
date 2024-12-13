10fc536cefde4348e9d6901d6ee71497ca8a6996
Artur Vinicius Pereira Fernandes <127784923+Arturvpf@users.noreply.github.com>
2024-12-13 03:06:09 -0300

    Update h_avpf.hs

--- PLC/Lista1/h_avpf.hs
+++ PLC/Lista1/h_avpf.hs
@@ -3,9 +3,12 @@ import Data.Char (digitToInt)
 isReplica :: String -> Int -> Char -> Bool
 isReplica str x ch = str == replicate x ch
 
+main :: IO()
 main = do
   a <- getLine
   b <- getLine
   c <- getChar
   let result = isReplica a (read b) c
   print result
+
+  --Correct but take a look-----
