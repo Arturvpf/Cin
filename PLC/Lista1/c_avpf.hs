8a284f536da593633abe59fb0c6cb8ac669c05fd
Artur Vinicius Pereira Fernandes <127784923+Arturvpf@users.noreply.github.com>
2024-12-13 18:31:32 -0300

    Update c_avpf.hs

--- PLC/Lista1/c_avpf.hs
+++ PLC/Lista1/c_avpf.hs
@@ -1,10 +1,9 @@
--- Função para suavizar uma lista de floats
 suaviza :: [Float] -> [Float]
+suaviza [] = []
+suaviza [x] = [x]
 suaviza xs = head xs : [ (xs !! (i - 1) + xs !! i + xs !! (i + 1)) / 3 | i <- [1..length xs - 2] ] ++ [last xs]
 
--- Função principal para interagir com entrada e saída
 main :: IO ()
 main = do
   lista <- getLine
   print $ suaviza (read lista :: [Float])
------Correct------
