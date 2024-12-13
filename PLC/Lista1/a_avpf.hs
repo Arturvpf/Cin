2bacac92f9b792f82921addfdd5d36d965a2e7bd
Artur Vinicius Pereira Fernandes <127784923+Arturvpf@users.noreply.github.com>
2024-12-13 17:03:12 -0300

    Update a_avpf.hs

--- PLC/Lista1/a_avpf.hs
+++ PLC/Lista1/a_avpf.hs
@@ -1,35 +1,30 @@
--- transformar tudo que é ponto e virgula em espaco ok
--- separar string por espaco ok
--- fazer uma tupla com a string e o double 
--- comparar a  sigla com a string ok
--- somar os doubles que forem iguais ok 
+changeChar :: String -> String 
+changeChar [] = []
+changeChar (x:xs) | x == ';' = ' ' : changeChar xs
+                       | otherwise = x : changeChar xs
 
-trocarCaractere :: String -> String --ok
-trocarCaractere [] = []
-trocarCaractere (a:as) | a == ';' = ' ' : trocarCaractere as
-                       | otherwise = a : trocarCaractere as
+divStr :: String -> [String] 
+divStr []  = []
+divStr str = words str
 
-dividirString2 :: String -> [String] --funcionando certo
-dividirString2 []  = []
-dividirString2 str = words str
+tupla :: [String] -> [(String, Double)] 
+tupla [] = []
+tupla (a:b:c:d:xs) = (b, read d) : tupla xs
 
-criarTupla :: [String] -> [(String, Double)] -- cria uma lista de tuplas
-criarTupla [] = []
-criarTupla (a:b:c:d:xs) = (b, read d) : criarTupla xs
-
-verificacao :: String -> [(String, Double)] -> [Double]
-verificacao _ [] = []
-verificacao sigla ((a,b):as) | a == sigla = b : verificacao sigla as
-                             | otherwise  = verificacao sigla as
+verif :: String -> [(String, Double)] -> [Double]
+verif y [] = []
+verif sigla ((a,b):xs) | a == sigla = b : verif sigla xs
+                              | otherwise  = verif sigla xs
 
 logMes :: String -> String -> Double
 logMes sigla frase | null sigla || null frase = 0.0
-                   | otherwise = foldl (+) 0 (verificacao sigla (criarTupla (dividirString2 (trocarCaractere frase))))
+                                                | otherwise = foldl (+) 0 (verif sigla (tupla (divStr (changeChar frase))))
 
 main = do
     a <- getLine
     b <- getLine
     let result = logMes a b
     print result
+
 --JAN
 --14 JAN;Amazon;40.32;15 JAN;Uber;14.84;25 JAN;Uber;34.24;02 FEV;Spotify;8.50;06 FEV;Uber;6.94;05 MAR;Burger;29.90;10 MAR;Burger;24.99;15 MAR;Uci;19.00;08 ABR;Itunes;3.50;13 ABR;Picpay;20.00;
