bb6aaa3fc77d042d2176c9888247468b1fc7a52d
Artur Vinicius Pereira Fernandes <127784923+Arturvpf@users.noreply.github.com>
2024-12-16 12:18:38 -0300

    Update i_avpf.hs

--- PLC/Lista1/i_avpf.hs
+++ PLC/Lista1/i_avpf.hs
@@ -1,19 +1,51 @@
-battleResult :: Int -> String
-battleResult cp
-  | (ab - cd)^2 < abcd = "Charmander vitorioso"
-  | otherwise          = "Charmander derrotado"
-  where
-    digits = map (read . (:[])) (show cp) -- Converte o número em uma lista de dígitos
-    [a, b, c, d] = digits                 -- Extrai os dígitos
-    ab = 10 * a + b                       -- Calcula AB
-    cd = 10 * c + d                       -- Calcula CD
-    abcd = cp                             -- ABCD é o próprio CP
+{-Problem Statement
+
+Biu capturou um Charmander! Ele descobriu que seu pokemón tem uma habilidade especial, depois de algumas batalhas que ele leva vantagem contra pokemóns que possui CPs(Combat Power) determinados. Para o Charmander vencer a batalha, seu oponente deve ter CP com seguintes características:
+
+CP deve possuir 4 dígitos; Sendo o CP representado pelas letras ABCD, onde cada letra representa um dígito, O Charmander só vencerá a partida caso (AB - CD)^2 < (ABCD) essas características estiverem presentes.
+
+Sua tarefa é escrever um programa que ajude Biu a escolher melhor os oponentes para que seu Charmander saia sempre vitorioso.
+
+Input
+
+C - número inteiro positivo.
+
+A linha única de entrada é composta pelo CP do oponente.
+
+Output
+
+R - string
+
+A linha única de saída é composta pelo resultado da batalha, com uma das mensagens:
+
+"Charmander vitorioso" "Charmander derrotado"
+
+Examples
+
+Case: 1
+
+Input
+
+5321
+
+Output
+
+"Charmander derrotado"-}
+
+import Data.Char (digitToInt)
+
+result :: Char -> Char -> Char -> Char -> String
+result x y z a 
+    | (charToi x * charToi y - charToi z * charToi a)^2 < charToi x * charToi y * charToi z * charToi a = "Charmander vitorioso"
+    | otherwise = "Charmander derrotado"
+    where 
+        charToi c = digitToInt c
 
 main :: IO ()
 main = do
-  input <- getLine
-  let cp = read input :: Int
-  putStrLn $ battleResult cp
-
-
---Errado Output--
+    a <- getChar
+    b <- getChar
+    c <- getChar
+    d <- getChar
+    let resultado = result a b c d
+    print resultado
