7296b0b567daaa064e544e87f3061c4ef2b7cad3
Artur Vinicius Pereira Fernandes <127784923+Arturvpf@users.noreply.github.com>
2024-12-16 12:15:02 -0300

    Update c_avpf.hs

--- PLC/Lista1/c_avpf.hs
+++ PLC/Lista1/c_avpf.hs
@@ -1,3 +1,45 @@
+{-Problem Statement
+
+No contexto de análise de dados, suavizar os dados resulta numa sequência de pontos onde o ruído previamente existente é minimizado. Uma técnica básica de suavização é dada por trocar um ponto no meio da sequência pelo valor da média dele que é computada como (ponto predecessor + ponto atual + ponto sucessor)/3, implemente essa técnica.
+
+OBS: note o termo "ponto no meio da sequência" isso implica que o ponto inicial e final não tem seus valores alterados
+
+suaviza :: [Float] -> [Float]
+Sugestão de Main
+
+main = do
+        lista <- getLine
+        print $ suaviza (read lista :: [Float])
+Input
+
+Lista de Floats
+
+Output
+
+Lista de Floats
+
+Examples
+
+Case: 1
+
+Input
+
+[30,20,10]
+
+Output
+
+[30.0,20.0,10.0]
+
+Case: 2
+
+Input
+
+[30,20,10,10,20]
+
+Output
+
+[30.0,20.0,13.333333,13.333333,20.0]-}
+
 suaviza :: [Float] -> [Float]
 suaviza [] = []
 suaviza [x] = [x]
