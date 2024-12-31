dddcd2bff1170c9123a9205fbf5e233a4092e485
Artur Vinicius Pereira Fernandes <127784923+Arturvpf@users.noreply.github.com>
2024-12-31 15:49:43 -0300

    Update D.hs

--- PLC/Lista2/D.hs
+++ PLC/Lista2/D.hs
@@ -1,11 +1,11 @@
 
-toLowerChar :: Char -> Char
-toLowerChar c
+lowChar :: Char -> Char
+lowChar c
     | 'A' <= c && c <= 'Z' = toEnum (fromEnum c + 32)
     | otherwise            = c
     
-toLowerString :: String -> String
-toLowerString = map toLowerChar
+lowString :: String -> String
+lowString = map lowChar
 
 bSort :: [String] -> [String]
 bSort []     = []
@@ -13,21 +13,19 @@ bSort (x:xs) = bSort [y | y <- xs , y < x] ++ [x] ++ bSort [y | y <- xs, y >= x]
 
 uncommonFromTwoSentences :: String -> String -> [String]
 uncommonFromTwoSentences [][] = []
-uncommonFromTwoSentences [] str2 = bSort (palavrasUnicas(words (toLowerString str2))) -- se a string 1 for vazia
-uncommonFromTwoSentences str1 [] = bSort (palavrasUnicas(words (toLowerString str1))) -- se a string 2 for vazia 
-uncommonFromTwoSentences str1 str2 = bSort (palavrasUnicas ((words (toLowerString str2)) ++ (words (toLowerString str1))) )
+uncommonFromTwoSentences [] str2 = bSort (unicWord(words (lowString str2))) -- se a string 1 for vazia
+uncommonFromTwoSentences str1 [] = bSort (unicWord(words (lowString str1))) -- se a string 2 for vazia 
+uncommonFromTwoSentences str1 str2 = bSort (unicWord ((words (lowString str2)) ++ (words (lowString str1))) )
 
 contaOcorrencias:: String -> [String] -> Int
 contaOcorrencias palavra lista = length (filter (== palavra) lista)
 
-palavrasUnicas :: [String] -> [String]
-palavrasUnicas [] = []
-palavrasUnicas lista = [palavra | palavra <- lista, contaOcorrencias palavra lista == 1] 
+unicWord :: [String] -> [String]
+unicWord [] = []
+unicWord lista = [palavra | palavra <- lista, contaOcorrencias palavra lista == 1] 
 
 main = do
 sentence_1 <- getLine
 sentence_2 <- getLine
 let result = uncommonFromTwoSentences sentence_1 sentence_2
 print result
-
-
