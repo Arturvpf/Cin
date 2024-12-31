5ece0a0a7ed6597f89c4707f9e01060b73f965ba
Artur Vinicius Pereira Fernandes <127784923+Arturvpf@users.noreply.github.com>
2024-12-31 17:10:10 -0300

    Update G.hs

--- PLC/Lista2/G.hs
+++ PLC/Lista2/G.hs
@@ -1,51 +1,3 @@
-{-Problem Statement
-
-Dados os seguintes tipos algébricos
-
-data Ops = SUM | MUL | SUB
-           deriving (Read)
-
-data IntTree = Nilt Int |
-               Node Ops IntTree IntTree
-               deriving (Read)
-Escreva a função
-
-evalTree :: IntTree -> Int
-que calcula o valor resultante das operações na árvore dada.
-
-As operações podem ser:
-
-SUM = Soma
-
-SUB = Subtração
-
-MUL = Multiplicação
-
-Utilize a seguinte função main para chamar evalTree:
-
-main = do
-    s <- getLine
-    let result = evalTree (read s)
-    print result
-Input
-
-Um IntTree
-
-Output
-
-Um Int
-
-Examples
-
-Case: 1
-
-Input
-
-(Node SUM (Node MUL (Nilt 5) (Nilt 3)) (Node SUB (Nilt 10) (Nilt 5)))
-
-Output
-
-20-}
 data Ops = SUM | MUL | SUB
            deriving (Read, Eq)
 
@@ -54,20 +6,12 @@ data IntTree = Nilt Int |
                deriving (Read)
 
 evalTree :: IntTree -> Int
-evalTree (Nilt x) = x -- se so tiver um valor retorna ele 
-evalTree (Node operacao esq dir) | operacao == SUM = (evalTree esq) + (evalTree dir)
-                                 | operacao == MUL = (evalTree esq) * (evalTree dir)
-                                 | operacao == SUB = (evalTree esq) - (evalTree dir)
+evalTree (Nilt x) = x 
+evalTree (Node operation esq dir) | operation == SUM = (evalTree esq) + (evalTree dir)
+                                 | operation == MUL = (evalTree esq) * (evalTree dir)
+                                 | operation == SUB = (evalTree esq) - (evalTree dir)
 
 main = do
     s <- getLine
     let result = evalTree (read s)
     print result
-
-
--- precisa comecar a fazer as operacoes das folhas para a raiz
--- faz a op do primeiro ramo
--- depois faz do segundo
--- depois faz dos resultados
-
--- precisa fazer um percurso pos ordem e ir fazendo as operacoes
